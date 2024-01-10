b35506d8728e3704339f73a2c3ad46422bbbcf76
Ahmad Nauval Syahputra <anvalputra@gmail.com>
2024-01-10 20:32:52 +0700

    sched: Tune sched_min_granularity
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -108,10 +108,10 @@ enum sched_tunable_scaling sysctl_sched_tunable_scaling
 
 /*
  * Minimal preemption granularity for CPU-bound tasks:
- * (default: 0.75 msec * (1 + ilog(ncpus)), units: nanoseconds)
+ * (default: 0.8 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_min_granularity = 750000ULL;
-unsigned int normalized_sysctl_sched_min_granularity = 750000ULL;
+unsigned int sysctl_sched_min_granularity = 800000ULL;
+unsigned int normalized_sysctl_sched_min_granularity = 800000ULL;
 
 /*
  * is kept at sysctl_sched_latency / sysctl_sched_min_granularity
